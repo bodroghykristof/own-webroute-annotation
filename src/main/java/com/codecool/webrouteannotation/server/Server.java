a798bc4409c3319610ec2a1142c25f12a8502c16
bodroghykristof <bodroghykristof@gmail.com>
2020-11-27 11:57:36 +0100

    Create 3 custom endpoints in Routes and use them in server

--- src/main/java/com/codecool/webrouteannotation/server/Server.java
+++ src/main/java/com/codecool/webrouteannotation/server/Server.java
@@ -1,5 +1,6 @@
 package com.codecool.webrouteannotation.server;
 
+import com.codecool.webrouteannotation.routes.Routes;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
 
@@ -11,14 +12,9 @@ public class Server {
 
 	public static void main(String[] args) throws IOException {
 		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-		server.createContext("/test", (HttpExchange t) -> {
-			String response = "This is the response";
-			System.out.println("Hey");
-			t.sendResponseHeaders(200, response.length());
-			OutputStream os = t.getResponseBody();
-			os.write(response.getBytes());
-			os.close();
-		});
+		server.createContext("/list", Routes::list);
+		server.createContext("/find", Routes::find);
+		server.createContext("/delete", Routes::delete);
 		server.setExecutor(null);
 		server.start();
 	}
