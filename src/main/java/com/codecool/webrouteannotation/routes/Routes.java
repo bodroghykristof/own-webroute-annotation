8eab506ca04e040f0cd56b8ed21dd6757bfb7c29
bodroghykristof <bodroghykristof@gmail.com>
2020-11-27 12:02:48 +0100

    Use annotation on methods in Routes

--- src/main/java/com/codecool/webrouteannotation/routes/Routes.java
+++ src/main/java/com/codecool/webrouteannotation/routes/Routes.java
@@ -1,5 +1,6 @@
 package com.codecool.webrouteannotation.routes;
 
+import com.codecool.webrouteannotation.annotation.WebRoute;
 import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
@@ -7,16 +8,19 @@ import java.io.OutputStream;
 
 public class Routes {
 
+	@WebRoute(path = "/list", method = "GET")
 	public static void list(HttpExchange httpExchange) throws IOException {
 		String response = "This is the list of elements";
 		sendResponse(httpExchange, response, 200);
 	}
 
+	@WebRoute(path = "/find", method = "GET")
 	public static void find(HttpExchange httpExchange) throws IOException {
 		String response = "Element was found";
 		sendResponse(httpExchange, response, 200);
 	}
 
+	@WebRoute(path = "/delete", method = "GET")
 	public static void delete(HttpExchange httpExchange) throws IOException {
 		String response = "Element was removed";
 		sendResponse(httpExchange, response, 202);
