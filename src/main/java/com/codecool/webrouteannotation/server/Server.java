8365b16a7f0c864c423dc14065261dd8a599a004
bodroghykristof <bodroghykristof@gmail.com>
2020-11-27 11:50:44 +0100

    Create Server class for handling incoming requests - handle one endpoint

--- src/main/java/com/codecool/webrouteannotation/server/Server.java
+++ src/main/java/com/codecool/webrouteannotation/server/Server.java
@@ -1,4 +1,26 @@
 package com.codecool.webrouteannotation.server;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class Server {
+
+	public static void main(String[] args) throws IOException {
+		HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+		server.createContext("/test", (HttpExchange t) -> {
+			String response = "This is the response";
+			System.out.println("Hey");
+			t.sendResponseHeaders(200, response.length());
+			OutputStream os = t.getResponseBody();
+			os.write(response.getBytes());
+			os.close();
+		});
+		server.setExecutor(null);
+		server.start();
+	}
+
 }
