485549ea9340f6a090abdf4a45e483444a1ad430
bodroghykristof <bodroghykristof@gmail.com>
2020-11-27 12:00:35 +0100

    Create basic characteristics of WebRoute annotation

--- src/main/java/com/codecool/webrouteannotation/annotation/WebRoute.java
+++ src/main/java/com/codecool/webrouteannotation/annotation/WebRoute.java
@@ -1,4 +1,16 @@
 package com.codecool.webrouteannotation.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface WebRoute {
+
+	public String path();
+	public String method();
+	public boolean active() default true;
+
 }
